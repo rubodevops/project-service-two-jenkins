fb1e916722688c98d598aaeae933337d334695b7
rubodevops <javarubo@gmail.com>
2021-07-16 06:00:41 +0400

    change

--- ec2.tf
+++ ec2.tf
@@ -25,7 +25,9 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-2"
-  shared_credentials_file = "/home/dev/Downloads/rootkey.csv"
+  access_key    = ${AWS_ACCESS_KEY_ID}
+  secret_key    = ${AWS_SECRET_ACCESS_KEY}
+  #shared_credentials_file = "/home/dev/Downloads/rootkey.csv"
   #profile                 = "default"
 }
 
